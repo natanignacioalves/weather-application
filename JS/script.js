715d366a582a646aba87a9f2fe96ce124fc1251c
Natan Alves <108841058+natanignacioalves@users.noreply.github.com>
2022-11-07 21:01:59 -0300

    Update

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+//Variáveis e Seleção de elementos
+const apiKey = "f38c26d40763ac6bc76abfcafdec883d";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+//Funções
+
+
+
+//Eventos
+  
+searchBtn.addEventListener("Click", (e) => {
+    e.preventDefault();
+
+    console.log("teste");
+});  //34min -----------aplicação de Clima com OpenWeather API - Projeto de JavaScript com API
