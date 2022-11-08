771942b2199455a957a1e22946fc35b97b2ea39b
Natan Alves <108841058+natanignacioalves@users.noreply.github.com>
2022-11-08 14:53:42 -0300

    update-project

--- JS/script.js
+++ JS/script.js
@@ -5,14 +5,50 @@ const apiCountryURL = "https://countryflagsapi.com/png/";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+
 //Funções
 
+const getwhatherData = async(city) => {  //async-assíncrona - executa tarefa em segundo plano
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`; 
+
+    const res = await fetch (apiWeatherURL);
+    const data = await res.json();
+
+    return data;
+}  
+
+const showWeatherData = async (city) => {
+    const data = await getwhatherData(city);
+
+    cityElement.innerText = data.name; // innerText 
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+      ); 
+      countryElement.setAttribute("src", apiCountryURL + data.sys.country); 
+      umidityElement.innerText = `${data.main.humidity}%`;  //-----------------------B.O    
+      windElement.innerText = `${data.wind.speed} km/h`;
+};
 
 
 //Eventos
   
-searchBtn.addEventListener("Click", (e) => {
+searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
+  
+    const city = cityInput.value;
 
-    console.log("teste");
-});  //34min -----------aplicação de Clima com OpenWeather API - Projeto de JavaScript com API
+    showWeatherData(city);
+  });
+  
